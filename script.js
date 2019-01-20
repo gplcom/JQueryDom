9dcff3a56681e8a8f0aff7ba2b1ed3e66b9a3cba
PASCALE <45562816+gplcom@users.noreply.github.com>
2019-01-20 23:46:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-var cercle = document.querySelectorAll(".cercle");
-for (var i = 0; i < cercle.length; i++) {
-	cercle[i].onclick = function(){
-		var idDeMaDiv = this.id;
-		if(!this.classList.contains("active")){
-		for (var j = 0; j < cercle.length; j++) {
-	cercle[j].classList.remove("active");
-		var idTmp = cercle[j].id;
-		document.getElementById("description_"+idTmp).style.display = "none";		
-		}
-		this.classList.add("active");		
-		document.getElementById("description_"+idDeMaDiv).style.display = "block";
-		} else {
-		this.classList.remove("active");
-		document.getElementById("description_"+idDeMaDiv).style.display = "none";
-		}
-	}
+$( document ).ready(function() {	
+		$("#bloc1").on({
+	    			//quand la souris entre, cet element se voit retiré la classe vert et ajouté la bleu
+	    			mouseenter:function(){
+	    			$(this).removeClass("vert").addClass("bleu");
+	    			//quand la souris sort, cet element se voit retiré la classe bleu et ajouté la vert
+	    		},mouseleave:function(){
+	    			$(this).removeClass("bleu").addClass("vert");}
+	    		});
+		
+//exo2
+$("#champ1").on("keyup",(function(){
+	$("#para1").text($("#champ1").val());
+}));
+//exo3
+	$(".bloc2").hover(function(){
+	    
+	if($.isNumeric($(this).text())){ //si la valeur est numerique alors 
+		if($(this).text()%2 ==0){ //analyser le texte avec un modulo 
+			$(this).addClass("vert");
+}else{
+	$(this).addClass("rouge");
 }
-
+}else{
+	$(this).addClass("bleu");
+}
+},function(){
+$(this).removeClass("vert bleu rouge");
+})
+		});
 
 
 
